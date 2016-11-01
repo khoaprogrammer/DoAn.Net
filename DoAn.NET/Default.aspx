2be45c76f370d2d183f0f74b4ceafa7df41f8555
NgoKhoa-PC <NgoKhoa-PC@DESKTOP-2P622M1>
2016-11-01 13:25:52 +0700

    Add project files.

---
+++ DoAn.NET/Default.aspx
@@ -0,0 +1,2 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/DisplayContent.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="DoAn.NET.Default" %>
+
---
+++ DoAn.NET/Models/Image.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAn.NET.Models
+{
+    public class Image
+    {
+        public string Name { get; set; }
+        public double Size { get; set; }
+        public DateTime UploadDate { get; set; }
+    }
+}
